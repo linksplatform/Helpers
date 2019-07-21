131bb1904dfe9b2a9947ebd1bc02eca05375984d
Константин Дьяченко <konard@me.com>
2019-07-21 16:54:25 +0300

    Fixed two more CodeFactor issues

--- Default.cs
+++ Default.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
 namespace Platform.Helpers
 {
-
-
     public class Default
     {
         public static void InitializeThreadInstance<T>()
