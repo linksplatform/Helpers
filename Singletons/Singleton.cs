92433c2eeb4faf536a7d74e550fea01fb22e986d
Константин Дьяченко <konard@me.com>
2019-07-26 20:28:45 +0300

    Singleton.cs file split in to two Singleton.cs and Singleton[T].cs

--- Singletons/Singleton.cs
+++ Singletons/Singleton.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Concurrent;
-using System.Reflection;
 using Platform.Interfaces;
-using Platform.Collections.Lists;
-using Platform.Reflection;
 
 namespace Platform.Helpers.Singletons
 {
@@ -16,25 +12,5 @@ namespace Platform.Helpers.Singletons
         public static T Get<T>(Func<T> creator) => new Singleton<T>(creator).Instance;
 
         public static T Get<T>(IFactory<T> factory) => new Singleton<T>(factory.Create).Instance;
-    }
-
-    public struct Singleton<T>
-    {
-        private static readonly ConcurrentDictionary<Func<T>, byte[]> Functions = new ConcurrentDictionary<Func<T>, byte[]>();
-        private static readonly ConcurrentDictionary<byte[], T> Singletons = new ConcurrentDictionary<byte[], T>(Default<IListEqualityComparer<byte>>.Instance);
-
-        private readonly Func<T> _creator;
-
-        public Singleton(Func<T> creator) => _creator = creator;
-
-        public T Instance
-        {
-            get
-            {
-                var creatorCopy = _creator;
-                var bytes = Functions.GetOrAdd(creatorCopy, creatorCopy.GetMethodInfo().GetILBytes());
-                return Singletons.GetOrAdd(bytes, key => creatorCopy());
-            }
-        }
     }
 }
---
+++ Singletons/Singleton[T].cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Reflection;
+using Platform.Collections.Lists;
+using Platform.Reflection;
+
+namespace Platform.Helpers.Singletons
+{
+    public struct Singleton<T>
+    {
+        private static readonly ConcurrentDictionary<Func<T>, byte[]> Functions = new ConcurrentDictionary<Func<T>, byte[]>();
+        private static readonly ConcurrentDictionary<byte[], T> Singletons = new ConcurrentDictionary<byte[], T>(Default<IListEqualityComparer<byte>>.Instance);
+
+        private readonly Func<T> _creator;
+
+        public Singleton(Func<T> creator) => _creator = creator;
+
+        public T Instance
+        {
+            get
+            {
+                var creatorCopy = _creator;
+                var bytes = Functions.GetOrAdd(creatorCopy, creatorCopy.GetMethodInfo().GetILBytes());
+                return Singletons.GetOrAdd(bytes, key => creatorCopy());
+            }
+        }
+    }
+}
