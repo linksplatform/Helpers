f805b52699101ac9a77edfedd7e4f00decaf4566
Константин Дьяченко <konard@me.com>
2019-07-26 20:08:31 +0300

    Default classes are static now.

--- Default.cs
+++ Default.cs
@@ -3,17 +3,15 @@ using System.Runtime.CompilerServices;
 
 namespace Platform.Helpers
 {
-    public class Default
+    public static class Default
     {
-        public static void InitializeThreadInstance<T>()
-            where T : new() => Default<T>.ThreadInstance = new T();
+        public static void InitializeThreadInstance<T>() where T : new() => Default<T>.ThreadInstance = new T();
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static T GetOrCreateThreadInstance<T>()
-            where T : class, new() => Default<T>.ThreadInstance ?? (Default<T>.ThreadInstance = new T());
+        public static T GetOrCreateThreadInstance<T>() where T : class, new() => Default<T>.ThreadInstance ?? (Default<T>.ThreadInstance = new T());
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static T GetOrCreateThreadInstance<T>(T nullValue)
-            where T : struct => EqualityComparer<T>.Default.Equals(Default<T>.ThreadInstance, nullValue) ? (Default<T>.ThreadInstance = new T()) : Default<T>.ThreadInstance;
+        public static T GetOrCreateThreadInstance<T>(T nullValue) where T : struct 
+            => EqualityComparer<T>.Default.Equals(Default<T>.ThreadInstance, nullValue) ? (Default<T>.ThreadInstance = new T()) : Default<T>.ThreadInstance;
     }
 }
--- Default[T].cs
+++ Default[T].cs
@@ -6,7 +6,7 @@ namespace Platform.Helpers
     /// Представляет собой точку доступа к экземплярям типов по умолчанию (созданных с помощью конструктора без аргументов).
     /// </summary>
     /// <typeparam name="T">Тип экземпляра объекта.</typeparam>
-    public class Default<T>
+    public static class Default<T>
         where T : new()
     {
         /// <summary>
