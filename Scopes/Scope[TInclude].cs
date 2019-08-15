9ef0d53b14996acef20181c5c023456ccd97f5b4
Константин Дьяченко <konard@me.com>
2019-08-15 09:43:50 +0300

    Platform.Helpers.Scopes namespace moved to separate package Platform.Scopes at nuget.org (https://github.com/linksplatform/Scopes). Closes #13.

--- Scopes/Scope[TInclude].cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Platform.Helpers.Scopes
-{
-    public class Scope<TInclude> : Scope
-    {
-        public Scope() : this(false, false) { }
-        public Scope(bool autoInclude) : this(autoInclude, false) { }
-        public Scope(bool autoInclude, bool autoExplore) : base(autoInclude, autoExplore) => Include<TInclude>();
-    }
-}
